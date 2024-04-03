f2c33daa5ec74d43075a70f642112439651735ef
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-04-03 05:13:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
-  SESSION_NAME: process.env.SESSION_NAME || "Bot~"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://api:api@api.pldyojn.mongodb.net/?retryWrites=true&w=majority&appName=api",
+  SESSION_NAME: process.env.SESSION_NAME || "alpha~"
 };
