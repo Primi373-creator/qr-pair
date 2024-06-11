8abf309e9fe4669f5952546b7ff22c83ccb5d4ef
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-06-11 11:52:51 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,101 +1,105 @@
-const axios = require('axios');
-const { makeid } = require('./id');
-const express = require('express');
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
+const { makeid } = require("./lib/makeid");
+const Paste = require("./models/session");
+const {
+  makeWASocket,
+  delay,
+  useMultiFileAuthState,
+  fetchLatestBaileysVersion,
+  makeCacheableSignalKeyStore,
+} = require("@whiskeysockets/baileys");
+const logger = require("pino")({
+  timestamp: () => `,"time":"${new Date().toJSON()}"`,
+}).child({});
+logger.level = "silent";
+const NodeCache = require("node-cache");
 const router = express.Router();
-const { default: makeWASocket, Browsers, delay, useMultiFileAuthState, BufferJSON, fetchLatestBaileysVersion, PHONENUMBER_MCC, DisconnectReason, makeInMemoryStore, jidNormalizedUser, makeCacheableSignalKeyStore } = require("@whiskeysockets/baileys")
-const Pino = require("pino")
-const pino = require("pino")
-const NodeCache = require("node-cache")
-const chalk = require("chalk")
+const path = require("path");
+const tempFolderPath = path.join(__dirname, "temp");
 
-router.get('/', async (req, res) => {
-    const idd = makeid();
-    const id = 'alpha~' + idd;
-    let num = req.query.number;
-
-async function qr() {
-//------------------------------------------------------
-let { version, isLatest } = await fetchLatestBaileysVersion()
-const {  state, saveCreds } =await useMultiFileAuthState('./session/'+id)
-    const msgRetryCounterCache = new NodeCache() // for retry message, "waiting message"
-    const XeonBotInc = makeWASocket({
-        logger: pino({ level: 'silent' }),
-        printQRInTerminal: false, // popping up QR in terminal log
-      browser: Browsers.windows('Firefox'), // for this issues https://github.com/WhiskeySockets/Baileys/issues/328
-     auth: {
-         creds: state.creds,
-         keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
-      },
-      markOnlineOnConnect: true, // set false for offline
-      generateHighQualityLinkPreview: true, // make high preview link
-      getMessage: async (key) => {
-         let jid = jidNormalizedUser(key.remoteJid)
-         let msg = await store.loadMessage(jid, key.id)
-
-         return msg?.message || ""
-      },
-      msgRetryCounterCache, // Resolve waiting messages
-      defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
-   })
- if (!XeonBotInc.authState.creds.registered) {
-  let phoneNumber = num
-      if (!!phoneNumber) {
-         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
-      } else {
-      
-      }
-  setTimeout(async () => {
-         let code = await XeonBotInc.requestPairingCode(phoneNumber)
-         code = code?.match(/.{1,4}/g)?.join("-") || code
-         console.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
-      if (!res.headersSent) {
-                    await res.send({ code });
-                }
-      }, 3000)
- }
-XeonBotInc.ev.on("connection.update",async  (s) => {
-        const { connection, lastDisconnect } = s
-        if (connection == "open") {
-            await delay(1000 * 10)
-            await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: '*thanks for choosing alpha-md*\n*you have completed first step in deploying alpha*\n*have a great day ahead*' });
-            await delay(1000 * 10)
-            const folderPath = `./session/${id}/`;
-        const unique = fs.readFileSync(__dirname + `/session/${id}/creds.json`)
-        const c = Buffer.from(unique).toString('base64');
-        const response = await axios.get(`https://worthwhile-mandy-c-iph3r.koyeb.app/session/upload?content=${encodeURIComponent(c)}`);
-              console.log(response.data.id)                               
-         XeonBotInc.groupAcceptInvite("BGWpp9qySw81CGrqRM3ceg");
-       const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: response.data.id });
-       await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `*ᴅᴇᴀʀ ᴜsᴇʀ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ*\n*◕ ⚠️ ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ᴛʜɪs ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴀs ɪᴛ ᴄᴏɴᴛᴀɪɴs ʀᴇǫᴜɪʀᴇᴅ ᴅᴀᴛᴀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ᴀᴄᴄᴇss ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ*` }, {quoted: xeonses});
-       await delay(1000 * 2)
-       process.exit(0);
-        }
-        if (
-            connection === "close" &&
-            lastDisconnect &&
-            lastDisconnect.error &&
-            lastDisconnect.error.output.statusCode != 401
-        ) {
-            qr()
-        }
-    })
-    XeonBotInc.ev.on('creds.update', saveCreds)
-    XeonBotInc.ev.on("messages.upsert",  () => { })
+function removeFile(filePath) {
+  if (!fs.existsSync(filePath)) return false;
+  fs.rmSync(filePath, {
+    recursive: true,
+    force: true,
+  });
 }
-qr()
+
+router.get("/pair", async (req, res) => {
+  const id = makeid();
+  let num = req.query.number;
+
+  async function getpair() {
+    let { version } = await fetchLatestBaileysVersion();
+    const msgRetryCounterCache = new NodeCache();
+    const { state, saveCreds } = await useMultiFileAuthState(
+      path.join(tempFolderPath, id),
+    );
+    try {
+      const client = makeWASocket({
+        version,
+        logger,
+        printQRInTerminal: false,
+        auth: {
+          creds: state.creds,
+          keys: makeCacheableSignalKeyStore(state.keys, logger),
+        },
+        msgRetryCounterCache,
+        generateHighQualityLinkPreview: true,
+      });
+
+      if (!client.authState.creds.registered) {
+        await delay(1500);
+        num = num.replace(/[^0-9]/g, "");
+        const code = await client.requestPairingCode(num);
+        if (!res.headersSent) {
+          await res.send({ code });
+        }
+      }
+
+      client.ev.on("creds.update", saveCreds);
+      client.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect } = s;
+
+        if (connection == "open") {
+          await delay(5000);
+          await delay(5000);
+          const credsPath = path.join(tempFolderPath, id, "creds.json");
+          const unique = fs.readFileSync(credsPath);
+          const content = Buffer.from(unique).toString("base64");
+          const paste = new Paste({
+            id: id,
+            number: client.user.id,
+            banned: false,
+            content: content,
+          });
+          await paste.save();
+          await client.sendMessage(client.user.id, { text: id });
+          await delay(100);
+          await client.ws.close();
+          return await removeFile(path.join(tempFolderPath, id));
+        } else if (
+          connection === "close" &&
+          lastDisconnect &&
+          lastDisconnect.error &&
+          lastDisconnect.error.output.statusCode != 401
+        ) {
+          await delay(10000);
+          getpair();
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      console.log("service restated");
+      await removeFile(path.join(tempFolderPath, id));
+      if (!res.headersSent) {
+        await res.send({ code: "Service Unavailable" });
+      }
+    }
+  }
+
+  return await getpair();
 });
 
 module.exports = router;
-
-process.on('uncaughtException', function (err) {
-let e = String(err)
-if (e.includes("conflict")) return
-if (e.includes("not-authorized")) return
-if (e.includes("Socket connection timeout")) return
-if (e.includes("rate-overlimit")) return
-if (e.includes("Connection Closed")) return
-if (e.includes("Timed Out")) return
-if (e.includes("Value not found")) return
-console.log('Caught exception: ', err)
-})
