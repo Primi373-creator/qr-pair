6f18d888ea143cc22f8492d97832405f2db6ee29
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-06-11 11:55:07 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,85 +1,88 @@
-const axios = require('axios');
-const QRCode = require('qrcode');
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const { makeid } = require('./id');
+const QRCode = require("qrcode");
+const express = require("express");
 const pino = require("pino");
+const fs = require("fs");
+const { makeid } = require("./lib/makeid");
+const Paste = require("./models/session");
 const {
-    default: makeWASocket,
-    useMultiFileAuthState,
-    jidNormalizedUser,
-    Browsers,
-    delay,
-    makeInMemoryStore,
+  makeWASocket,
+  useMultiFileAuthState,
+  Browsers,
+  delay,
+  fetchLatestBaileysVersion,
 } = require("@whiskeysockets/baileys");
+const router = express.Router();
+const path = require("path");
+const tempFolderPath = path.join(__dirname, "temp");
 
-const { readFile } = require("node:fs/promises")
+function removeFile(filePath) {
+  if (!fs.existsSync(filePath)) return false;
+  fs.rmSync(filePath, {
+    recursive: true,
+    force: true,
+  });
+}
 
-let router = express.Router()
+router.get("/qr", async (req, res) => {
+  const id = makeid();
+  async function Getqr() {
+    const { state, saveCreds } = await useMultiFileAuthState(
+      path.join(tempFolderPath, id),
+    );
+    const { version } = await fetchLatestBaileysVersion();
+    try {
+      const client = makeWASocket({
+        version,
+        auth: state,
+        printQRInTerminal: false,
+        logger: pino({
+          level: "silent",
+        }),
+        browser: ["Chrome", "Ubuntu", "3.0"],
+      });
 
-function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, {
-        recursive: true,
-        force: true
-    })
-};
-
-router.get('/', async (req, res) => {
-    const id = makeid();
- async function Getqr() {
-        const {  state, saveCreds } =await useMultiFileAuthState('./session/'+id)
-        try {
-            let alpha = makeWASocket({
-                auth: state,
-                printQRInTerminal: false,
-                logger: pino({
-                    level: "silent"
-                }),
-                browser: Browsers.windows('Firefox'),
-            });
-
-            alpha.ev.on('creds.update', saveCreds)
-            alpha.ev.on("connection.update",async  (s) => {
-            const { connection, lastDisconnect, qr } = s;
-            if (qr) await res.end(await QRCode.toBuffer(qr));
-                if (connection == "open") {
-            await delay(1000 * 10)
-            await alpha.sendMessage(alpha.user.id, { text: '*thanks for choosing alpha-md*\n*your sesssionid will be sent in soon please wait..*\n*have a great day ahead*' });
-            await delay(1000 * 10)
-            const folderPath = `./session/${id}/`;
-        const unique = fs.readFileSync(__dirname + `/session/${id}/creds.json`)
-        const c = Buffer.from(unique).toString('base64');
-        const response = await axios.get(`https://worthwhile-mandy-c-iph3r.koyeb.app/session/upload?content=${encodeURIComponent(c)}`);
-              console.log(response.data.id)                               
-         alpha.groupAcceptInvite("BGWpp9qySw81CGrqRM3ceg");
-       const xeonses = await alpha.sendMessage(alpha.user.id, { text: response.data.id });
-       await alpha.sendMessage(alpha.user.id, { text: `*ᴅᴇᴀʀ ᴜsᴇʀ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ*\n*◕ ⚠️ ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ᴛʜɪs ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴀs ɪᴛ ᴄᴏɴᴛᴀɪɴs ʀᴇǫᴜɪʀᴇᴅ ᴅᴀᴛᴀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ᴀᴄᴄᴇss ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ*` }, {quoted: xeonses});
-       await delay(1000 * 2)
-       process.exit(0);
-        }
-        if (
-            connection === "close" &&
-            lastDisconnect &&
-            lastDisconnect.error &&
-            lastDisconnect.error.output.statusCode != 401
+      client.ev.on("creds.update", saveCreds);
+      client.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect, qr } = s;
+        if (qr) await res.end(await QRCode.toBuffer(qr));
+        if (connection == "open") {
+          await delay(5000);
+          await delay(5000);
+          const credsPath = path.join(tempFolderPath, id, "creds.json");
+          const unique = fs.readFileSync(credsPath);
+          const content = Buffer.from(unique).toString("base64");
+          const paste = new Paste({
+            id: id,
+            number: client.user.id,
+            banned: false,
+            content: content,
+          });
+          await paste.save();
+          await client.sendMessage(client.user.id, { text: id });
+          await delay(100);
+          await client.ws.close();
+          return await removeFile(path.join(tempFolderPath, id));
+        } else if (
+          connection === "close" &&
+          lastDisconnect &&
+          lastDisconnect.error &&
+          lastDisconnect.error.output.statusCode != 401
         ) {
-            Getqr();
-                }    
-            });
-            alpha.ev.on('creds.update', saveCreds);
-            alpha.ev.on("messages.upsert",  () => { });
-        } catch (err) {
-            if (!res.headersSent) {
-                await res.json({
-                    code: "Service Unavailable"
-                });
-            }
-            console.log(err);
+          await delay(10000);
+          Getqr();
         }
+      });
+    } catch (err) {
+      if (!res.headersSent) {
+        await res.json({
+          code: "Service Unavailable",
+        });
+      }
+      console.log(err);
+      await removeFile(path.join(tempFolderPath, id));
     }
-    return await Getqr()
+  }
+  return await Getqr();
 });
 
 module.exports = router;
